7c4cb216d050f5cb52de17b77165e106eb28af32
Kenyy0804 <159415549+Kenyy0804@users.noreply.github.com>
2024-06-06 15:31:22 +0900

    store

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UploadImageRequest;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image as ImageManager;
 
 class ImageController extends Controller
 {
@@ -31,7 +33,7 @@ class ImageController extends Controller
     public function index()
     {
         $images = Image::where('owner_id', Auth::id())
-        ->orderBy('update_at', 'desc')->paginate(20);
+        ->orderBy('updated_at', 'desc')->paginate(20);
 
         return view('owner.images.index', compact('images'));
     }
@@ -49,7 +51,32 @@ class ImageController extends Controller
      */
     public function store(UploadImageRequest $request)
     {
-        dd($request);
+        // $imageFile = $request->image;
+        $imageFiles = $request->file('files');
+        if(!is_null($imageFiles)) {
+            foreach($imageFiles as $imageFile) {
+                
+                if(is_array($imageFile))
+                {
+                    $file = $imageFile['image'];
+                } else {
+                    $file = $imageFile;
+                }
+                $fileName = uniqid(rand() . '_'); //ランダムでファイル名を作成し、_を付ける。
+                $extension = $file->extension(); // 拡張子を取得
+                $fileNameToStore = $fileName . '.' . $extension; // 作ったファイル名と拡張子を変数になおす。
+                $resizedImage = ImageManager::make($file)->resize(1920, 1080)->encode();
+    
+                $folderName = 'products';  // フォルダ名を指定
+                Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
+                Image::create([
+                    'owner_id' => Auth::id(),
+                    'filename' => $fileNameToStore
+                ]);
+            }
+        }
+        return redirect()->route('owner.images.index')
+        ->with(['message', '画像登録を実施しました。', 'status' => 'info']);
     }
 
     /**
--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -9,10 +9,16 @@ class ImageService
 {
     public static function upload($imageFile, $folderName)
     {
+        if(is_array($imageFile))
+        {
+            $file = $imageFile['image'];
+        } else {
+            $file = $imageFile;
+        }
             $fileName = uniqid(rand() . '_'); //ランダムでファイル名を作成し、_を付ける。
-            $extension = $imageFile->extension(); // 拡張子を取得
+            $extension = $file->extension(); // 拡張子を取得
             $fileNameToStore = $fileName . '.' . $extension; // 作ったファイル名と拡張子を変数になおす。
-            $resizedImage = Image::make($imageFile)->resize(1920, 1080)->encode();
+            $resizedImage = Image::make($file)->resize(1920, 1080)->encode();
 
             Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
 
