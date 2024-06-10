682843c2926367f2e1aea301db74c2e6bce5b56e
Kenyy0804 <159415549+Kenyy0804@users.noreply.github.com>
2024-06-10 13:53:06 +0900

    micromodal

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -28,7 +28,10 @@
 
                                 </div>
                             </div>
-
+                            <x-select-image :images="$images" name="image1" />
+                            <x-select-image :images="$images" name="image2" />
+                            <x-select-image :images="$images" name="image3" />
+                            <x-select-image :images="$images" name="image4" />
                             <div class="p-2 w-full flex justify-around">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                                 <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
@@ -39,4 +42,23 @@
             </div>
         </div>
     </div>
+    <script>
+        'use strict'
+        const images = document.querySelectorAll('.image') //全てのimageタグを取得
+
+        images.forEach( image => { //1行ずつ繰り返す
+            image.addEventListener('click', function(e){ //クリックしたら
+                const imageName = e.target.dataset.id.substr(0, 6) //data-idの6文字
+                const imageId = e.target.dataset.id.replace(imageName + '_', '') //6文字カット
+                const imageFile = e.target.dataset.file
+                const imagePath = e.target.dataset.path
+                const modal = e.target.dataset.modal
+                //サムネイルとinput type=hiddenのvalueに設定
+                document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
+                document.getElementById(imageName + '_hidden').value = imageId
+                MicroModal.close(modal); //モーダルを閉じる
+
+            })
+        })
+    </script>
 </x-app-layout>
