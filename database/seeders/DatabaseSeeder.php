b1f2378812c1e669eea637c9d564dff58511b1a3
Kenyy0804 <159415549+Kenyy0804@users.noreply.github.com>
2024-06-12 09:28:02 +0900

    route,ItemController,index

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             // ProductSeeder::class,
             // StockSeeder::class
+
+            UserSeeder::class
         ]);
     }
 }
