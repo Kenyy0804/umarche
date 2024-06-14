6478e30726bcca2210758dc837951ca98969688b
Kenyy0804 <159415549+Kenyy0804@users.noreply.github.com>
2024-06-14 09:21:21 +0900

    cart add

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\User\ItemController;
+use App\Http\Controllers\User\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,7 +26,10 @@ Route::middleware('auth:users')->group(function(){
     Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 });
 
-
+Route::prefix('cart')->
+middleware('auth:users')->group(function(){
+    Route::post('add', [CartController::class, 'add'])->name('cart.add');
+});
 
 
 
